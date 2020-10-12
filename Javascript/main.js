d188582f5e8c2099537a43616155358fac89f589
Cesar Gomez <cesargomez060690@gmail.com>
2020-10-12 12:23:35 -0300

    Agregue condicional mayorEdad al Js

--- Javascript/main.js
+++ Javascript/main.js
@@ -2,18 +2,21 @@ var deseasRegistrarte = prompt("¿Deseas registrarte para empezar a comprar?");
 
 var condicionFiscal = prompt("¿Posee monotributo o es Responsable Incripto?");
 
+var mayorEdad = prompt ("¿Que edad tenes?");
 
-
-
-if (condicionFiscal == "si" || "monotributo" || "responsable") {
+if (mayorEdad <= 17) {
+alert ("No puedes comprar siendo menor de edad")
+} 
+else if (condicionFiscal == "si" || "monotributo" || "responsable") {
 alert("Puedes empezar a comprar, solo te pediremos unos datos");
 var nombre = prompt("Ingrese su nombre");
 var apellido = prompt ("Ingrese su apellido");
 alert (nombre + " " + apellido + " " +"ahora si puedes empezar a comprar")
-}else {
+}
+else {
 alert("No puedes comprar, necesitas estar registrado en Afip");   
 }
 
-
+console.log(nombre+ " " + apellido)
 
 
