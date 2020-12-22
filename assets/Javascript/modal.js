0fa7dcfaed6bd6b2621fdc7558ed272e8efe8e6c
Cesar Gomez <cesargomez060690@gmail.com>
2020-12-22 10:07:09 -0300

    Agregue el carrito de compras como array y el push de los productos

--- assets/Javascript/modal.js
+++ assets/Javascript/modal.js
@@ -1,14 +1,17 @@
+// Variables necesarias
 let cerrar = document.querySelectorAll(".close")[0];
 let abrir = document.querySelectorAll(".cta")[0];
 let modal = document.querySelectorAll(".modal")[0];
 let modalC = document.querySelectorAll(".modal-container")[0];
 
+//Abrir modal de carrito
 abrir.addEventListener("click",function(e){
     e.preventDefault();
     modalC.style.opacity = "1";
     modalC.style.visibility = "visible";
     modal.classList.toggle("modal-close");
 });
+//Cerrar modal de carrito
 cerrar.addEventListener("click",function (params) {
     modal.classList.toggle("modal-close");
     
@@ -16,14 +19,4 @@ cerrar.addEventListener("click",function (params) {
         modalC.style.opacity = "0";
         modalC.style.visibility = "hidden";
     },900)
-});
-window.addEventListener("click",function(e) {
-    if(e.target == modalC){
-        modal.classList.toggle("modal-close");
-    
-        setTimeout(function() {
-            modalC.style.opacity = "0";
-            modalC.style.visibility = "hidden";
-        },900)
-    }
 });
--- assets/Javascript/productos.js
+++ assets/Javascript/productos.js
@@ -1,3 +1,4 @@
+//Agregue el Objeto constructor Producto
 function Producto (nombre,rodado,marca,precio,color) {
     this.nombre = nombre;
     this.rodado = rodado;
@@ -7,6 +8,9 @@ function Producto (nombre,rodado,marca,precio,color) {
 }
 const producto1 = new Producto("Bicicleta 1", 29, "Top Mega", 40000, "Amarillo");
 const producto2 = new Producto("Bicicleta 2", 29, "Top Mega", 35000, "Negro");
-console.log(producto1);
-console.log(producto2);
+const producto3 = new Producto("Bicicleta 3", 21, "Top Fire", 30000, "Azul");
+//console.log(producto1);
+//console.log(producto2);
+
+
 
